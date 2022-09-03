d4d0461b92083336098bc5c64a1aa2bd1c3aafcd
SEBROVATER <sebrovskiy.k@gmail.com>
2022-09-03 12:21:39 +0200

    optimised imports

--- bbox/__init__.py
+++ bbox/__init__.py
@@ -1,4 +1,2 @@
-from .bbox_methods import *
-from .int_bbox import IntBBox as BBox
-
-all = ["BBox", "sort_clockwise", "get_IoU", "get_distance", "get_cos_between", "non_max_suppression"]
+from bbox.bbox_methods import *
+from bbox.int_bbox import IntBBox
