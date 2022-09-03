3e60ca6b0baa0204aeb9ea2adc473bd7694b774a
SEBROVATER <sebrovskiy.k@gmail.com>
2022-09-03 13:12:37 +0200

    Revert "removed setup.py"
    This reverts commit 60587312e5620f90e831c0fbdeaf81499c4c7a32.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='bbox',
+    version='0.0.1',
+    author='Konstantin Sebrovskiy',
+    author_email='sebrovskiy.k@gmail.com',
+    description='Objected management of bbox',
+    url='https://github.com/SEBROVATER/bbox',
+    license='MIT',
+    packages=["bbox"],
+    install_requires=['numpy', "opencv-python", "imutils"],
+)
