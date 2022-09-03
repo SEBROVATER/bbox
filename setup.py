b1af9ea8588e9134c9493c009b5f71f6f9f9885c
SEBROVATER <sebrovskiy.k@gmail.com>
2022-09-03 19:15:25 +0200

    updated by pre-commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name='bbox',
-    version='0.0.1',
-    author='Konstantin Sebrovskiy',
-    author_email='sebrovskiy.k@gmail.com',
-    description='Objected management of bbox',
-    url='https://github.com/SEBROVATER/bbox',
-    license='MIT',
+    name="bbox",
+    version="0.0.1",
+    author="Konstantin Sebrovskiy",
+    author_email="sebrovskiy.k@gmail.com",
+    description="Objected management of bbox",
+    url="https://github.com/SEBROVATER/bbox",
+    license="MIT",
     packages=["bbox"],
-    # install_requires=['numpy', "opencv-python", "imutils"],
-    long_description=open('README.md').read(),
+    install_requires=["numpy", "opencv-python"],
+    long_description=open("README.md").read(),
 )
