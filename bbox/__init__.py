80be9c126e00ae8366981920e746cb0f2bf56459
SEBROVATER <sebrovskiy.k@gmail.com>
2022-09-04 00:31:45 +0200

    revert IntBBox and FloatBBox without dots

--- bbox/__init__.py
+++ bbox/__init__.py
@@ -1,5 +1,5 @@
 # from . import bbox_utils, types
-from .sources.float_bbox import FloatBBox
-from .sources.int_bbox import IntBBox
+from sources.float_bbox import FloatBBox
+from sources.int_bbox import IntBBox
 
 __all__ = ["IntBBox", "FloatBBox"]
